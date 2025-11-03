73b881f5df32cbdb88342cc598c86c897fb0093b
Andrew <akolomiec982@gmail.com>
2025-11-04 00:30:00 +0200

    Added swagger documentation using drf-spectacular

--- characters/urls.py
+++ characters/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from characters.views import get_random_characters_view
+from characters.views import get_random_characters_view, CharacterListView
 
 app_name = "characters"
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path(
         "characters/random/", get_random_characters_view, name="characters_random"
     ),  # енд поінт, який повертає випадковий персонаж із серіалу «Рік і Морті».
+    path(
+        "characters/", CharacterListView.as_view(), name="characters_list"
+    ),  # Кінцева точка, що отримує «search_string» (рядок_пошуку) як аргумент і повертає список усіх персонажів, ім'я яких містить search_string (рядок_пошуку).
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,4 +1,6 @@
-from rest_framework import status
+from django.db.models import QuerySet
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -8,13 +10,45 @@ from characters.serializers import CharacterSerializer
 
 
 # створемо кастомну вюху для енд поінта, яка повертає випадковий персонаж із серіалу «Рік і Морті».
+
+
+@extend_schema(responses={200: CharacterSerializer})
 @api_view(["GET"])
 def get_random_characters_view(request: Request) -> Response:
+    """Get random character from Rick & Morty world."""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 # Створимо дефолтний клас бейс вю для Кінцевої точки,
-# що отримує «search_string» як аргумент і повертає список усіх персонажів, ім'я яких містить search_string.
+# що отримує «search_string» (рядок_пошуку) як аргумент і повертає список усіх персонажів, ім'я яких містить search_string (рядок_пошуку).
+
+
+class CharacterListView(generics.ListAPIView):
+
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter characters by name insensitive contains",
+                required=False,
+                type=str,
+            ),
+        ]
+    )
+    # перевизначаємо метод get, який прописаний в ListAPIView:
+    def get(self, request, *args, **kwargs) -> Response:
+        """List characters with filter by name."""
+        return super().get(request, *args, **kwargs)
